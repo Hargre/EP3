9983d31fa335d3998ca72dc8e7a2e35cf5d3ba42
Felipe Hargreaves <fhargreaves00@gmail.com>
2016-11-30 23:24:58 -0200

    Create users controller and update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
-  root           'static_pages#home'
-  get 'about' => 'static_pages#about'
+  root              'static_pages#home'
+  get 'sobre'    => 'static_pages#about'
+  get 'cadastro' => 'users#new'
 end
