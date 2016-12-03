3a7843a4e5d8abe3bf0206a1574794adab47ae79
Felipe Hargreaves <fhargreaves00@gmail.com>
2016-12-03 12:34:18 -0200

    Implement administrative privileges

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,15 @@
 class UsersController < ApplicationController
+  before_action :logged_in_user, only: [:index, :destroy]
+  before_action :admin_user,     only: :destroy
+
   def show
     @user = User.find(params[:id])
   end
+
   def new
     @user = User.new
   end
+
   def create
     @user = User.new(user_params)
     if @user.save
@@ -15,8 +20,29 @@ class UsersController < ApplicationController
     end
   end
 
+  def destroy
+    User.find(params[:id]).destroy
+    flash[:success] = "Usuário removido"
+    redirect_to users_url
+  end
+
+  def index
+    @users = User.all
+  end
+
   private
     def user_params
       params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end
+
+    def logged_in_user
+      unless logged_in?
+        flash[:danger] = "Entre antes de realizar essa ação."
+        redirect_to entrar_url
+      end
+    end
+
+    def admin_user
+      redirect_to(root_url) unless current_user.admin?
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   root              'static_pages#home'
   get    'sobre'    => 'static_pages#about'
   get    'cadastro' => 'users#new'
+  get    'indice'   => 'users#index'
   get    'entrar'   => 'sessions#new'
   post   'entrar'   => 'sessions#create'
   delete 'sair'     => 'sessions#destroy'
