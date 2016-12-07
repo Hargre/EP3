a221d926386dbc219937d20ee2f00d4ad8ad40ce
Felipe Hargreaves <fhargreaves00@gmail.com>
2016-12-07 12:34:10 -0200

    Handle invalid logins

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,6 +8,7 @@ class SessionsController < ApplicationController
       log_in(user)
       redirect_to user
     else
+      flash.now[:danger] = 'Usuário ou senha incorretos!'
       render 'new'
     end
   end
